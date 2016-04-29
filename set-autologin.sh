2ac5f1bbe1ccef0e3587ca8caecc5ceb0936ef52
Karlson2k <k2k@narod.ru>
2016-04-29 15:06:20 +0300

    Do not interfere with environment

--- set-autologin.sh
+++ set-autologin.sh
@@ -64,6 +64,7 @@ if [ -f /etc/lxdm/lxdm.conf ]; then
   echo 'Found LXDM configuration.'
   modify_ok='no' || exit 5
   if egrep -q -e '^#?autologin=' /etc/lxdm/lxdm.conf; then
+    unset oldalname || exit 5
     if oldalname=\$(sed -n "s/^autologin=\(.*\)$/\1/1p" /etc/lxdm/lxdm.conf) && \
          [ -n "\$oldalname" ] ; then
       echo "LXDM was configured to automatically login user \"\$oldalname\"."
